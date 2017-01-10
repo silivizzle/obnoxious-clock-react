61303a833adcc9f275cf8368e1e36d59b3fd0b78
silivizzle <tiberius220@gmail.com>
2017-01-10 07:19:29 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Clock from './components/clock/clock';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Clock />
+      </div>
+    );
+  }
+}
+
+export default App;
